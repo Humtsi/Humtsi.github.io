ae74f45040370f0ffdddcffe008a3a8c2ef2b667
Quentin <quentin.paronneau@gmail.com>
2024-08-20 16:41:13 +0200

    page 404

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,10 +1,21 @@
-import { Link } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 import '../../styles/themes/global.scss'
 import '../../styles/components/error.scss'
 
 function Error() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Redirection vers la page d'accueil après un délai ou sur une action utilisateur
+    const timer = setTimeout(() => {
+      navigate('/');
+    }, 3000); // 3 secondes avant la redirection
+
+    return () => clearTimeout(timer);
+  }, [navigate]);
 
   return (
     <div className='errorContainer'>
@@ -14,9 +25,7 @@ function Error() {
         <div className='error__span'>
           <span>Oups! La page que vous&nbsp;demandez&nbsp;n'existe pas.</span>
         </div>
-        <Link className='error__link' to="/">
-          Retourner sur la page d'accueil
-        </Link>
+        <p>Vous allez être redirigé vers la page d'accueil...</p>
       </div>
       <Footer/>
     </div>
